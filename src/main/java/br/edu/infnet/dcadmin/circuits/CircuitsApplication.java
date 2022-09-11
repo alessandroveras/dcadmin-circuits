eb15d30678778288d67a4a69573a13becd1c8b50
alessandroveras <alessandro.veras@gmail.com>
2022-09-10 22:22:43 -0300

    first commit

---
+++ src/main/java/br/edu/infnet/dcadmin/circuits/CircuitsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.dcadmin.circuits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/dcadmin/circuits/CircuitsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.dcadmin.circuits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
